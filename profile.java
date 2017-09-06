a24e9c9bacad153beb0bfe6442cd6601bf49a1da
Mental-box <31638945+Mental-box@users.noreply.github.com>
2017-09-06 21:09:45 +0900

    Add files via upload

---
+++ profile.java
@@ -0,0 +1,12 @@
+package profile;
+
+public class profile {
+	public static void main(String args[]){
+		System.out.println("Student ID number : 2016301041");
+		System.out.println("name : ¼ÛÄ¡¼º");
+		System.out.println("mobile phone number : 010-5509-4505");
+		System.out.println("GitHub address : https://github.com/Mental-box");
+		
+	}
+
+}
